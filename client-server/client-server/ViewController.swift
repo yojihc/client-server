5b948a04a1b94749774f3766ff9b31afab9d706a
Valera <r3volution1@bk.ru>
2021-12-10 12:49:46 +0300

    Homework for lesson 01
    Added Singleton and two properties

---
+++ client-server/client-server/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  client-server
+//
+//  Created by yoji on 10.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
